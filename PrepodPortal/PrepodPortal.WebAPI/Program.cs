890e2f54cee626ffb2d8f80bff58bbbb6b2622c3
elkprostoelk <novikovmisha138138@gmail.com>
2022-12-28 00:13:00 +0200

    provided logging and exceptions handling

--- PrepodPortal/PrepodPortal.WebAPI/Program.cs
+++ PrepodPortal/PrepodPortal.WebAPI/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuredLogger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(configuredLogger);
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,6 +23,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    var logger = app.Logger;
+    app.UseExceptionHandler(appBuilder => appBuilder.Run(async context =>
+    {
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature.Error;
+        logger.LogCritical(exception, "An exception occured while processing the request");
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsJsonAsync(new { error = "Internal Server Error"});
+    }));
 }
 
 app.UseHttpsRedirection();
